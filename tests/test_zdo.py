5b9ec9749ace1b5e8e93c588ee2eb89e3dd22929
Miroslav Jirik <miroslav.jirik@gmail.com>
2022-05-30 14:20:20 +0200

    show images, ignore negative values

--- tests/test_zdo.py
+++ tests/test_zdo.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pytest
 import os
 import skimage.io
@@ -18,14 +19,16 @@ import logging
 # cd ZDO2022
 # python -m pytest
 
+
+# Nastavte si v operačním systém proměnnou prostředí 'ZDO_DATA_PATH' s cestou k datasetu.
+# Pokud není nastavena, využívá se testovací dataset tests/test_dataset
+dataset_path = Path(os.getenv('ZDO_PATH_', default=Path(__file__).parent / 'test_dataset/'))
+# dataset_path = Path(r"H:\biology\orig\zdo_varroa_detection_coco_001")
+# dataset_path = Path(r"G:\Můj disk\data\biomedical\orig\pigleg_surgery\ZDO2022")
+
 def test_run_random():
     import zdo2022.main
     vdd = zdo2022.main.InstrumentTracker()
-
-    # Nastavte si v operačním systém proměnnou prostředí 'ZDO_DATA_PATH' s cestou k datasetu.
-    # Pokud není nastavena, využívá se testovací dataset tests/test_dataset
-    dataset_path = Path(os.getenv('ZDO_PATH_', default=Path(__file__).parent / 'test_dataset/'))
-    # dataset_path = Path(r"H:\biology\orig\zdo_varroa_detection_coco_001")
 
     print(f'dataset_path = {dataset_path}')
     types = ('*.MP4', '*.mkv')  # the tuple of file types
@@ -58,10 +61,6 @@ def test_run_all():
     import zdo2022.main
     vdd = zdo2022.main.InstrumentTracker()
 
-    # Nastavte si v operačním systém proměnnou prostředí 'ZDO_DATA_PATH' s cestou k datasetu.
-    # Pokud není nastavena, využívá se testovací dataset tests/test_dataset
-    dataset_path = os.getenv('ZDO_DATA_PATH_', default=Path(__file__).parent / 'test_dataset/')
-    # dataset_path = Path(r"H:\biology\orig\zdo_varroa_detection_coco_001")
 
     # print(f'dataset_path = {dataset_path}')
     types = ('*.MP4', '*.mkv')  # the tuple of file types
@@ -71,33 +70,62 @@ def test_run_all():
 
     f1s = []
     for filename in files:
-        prediction = vdd.predict(str(filename))
+        ann_pth = filename.with_suffix(".xml")
+        if filename.exists() and ann_pth.exists():
+            prediction = vdd.predict(str(filename))
 
-        df_eval = check_one_prediction(filename, prediction)
-        f1s.append(df_eval)
+
+            df_eval = check_one_prediction(filename, prediction)
+            f1s.append(df_eval)
 
     df = pd.concat(f1s)
+    plt.show()
     # print(df)
     return df
     # assert f1 > 0.55
 
 
 def check_one_prediction(filename:Path, prediction:dict) -> pd.DataFrame:
+    import zdo2022
+    zdo_pth = Path(zdo2022.__file__).parent.parent
+    name = zdo_pth.name
     ann_pth = filename.with_suffix(".xml")
     assert ann_pth.exists()
     ground_true_annotation = xml_to_dict(ann_pth)
 
     df_gt = pd.DataFrame(ground_true_annotation)
+    label_map = {
+        "needle holder" : 0,
+        "scissors": 1,
+        "tweezers": 2
+    }
+    df_gt["object_id"] = df_gt.label.map(lambda oid: label_map[oid])
     # df_gt.frame_id = pd.to_numeric(df_gt.frame_id)
     # df_gt.object_id = pd.to_numeric(df_gt.object_id)
     df_prediction = pd.DataFrame(prediction)
+    df_prediction = df_prediction[df_prediction.x_px >= 0] # filter out negative values
     # df_prediction.frame_id = pd.to_numeric(df_prediction.frame_id)
     # df_prediction.object_id = pd.to_numeric(df_prediction.object_id)
 
-    print(df_prediction)
+    df_prediction.to_csv(f"{name}_{filename.name}_prediction.csv")
+    # print(df_prediction)
+    #
+    # print(df_gt)
+    import seaborn as sns
+    # import ipdb
+    # ipdb.set_trace()
+    df_gt["ground_true"] = True
+    df_prediction["ground_true"] = False
+    dfall = pd.concat([df_gt, df_prediction], axis=0)
+    dfall = dfall.reset_index()
 
-    print(df_gt)
     df_eval = dist_eval(df_gt, df_prediction)
+
+    df_eval.to_csv(f"{name}_{filename.name}_eval.csv")
+    dfall.to_csv(f"{name}_{filename.name}_all.csv")
+    plt.figure()
+    sns.lineplot(data=dfall[dfall.object_id == 0], x="x_px", y="y_px", hue="ground_true")
+    plt.savefig(f"{name}_{filename.name}_trajectory.pdf")
 
     return df_eval
 
@@ -111,6 +139,7 @@ def xml_to_dict(pth:Path):
         "x_px": [],  # x pozice obarvených hrotů v pixelech
         "y_px": [],  # y pozice obarvených hrotů v pixelech
         "annotation_timestamp": [],
+        "label": [],
     }
 
     tree = etree.parse(pth)
@@ -124,6 +153,7 @@ def xml_to_dict(pth:Path):
             # annotation["filename"].append(str(pth))
             annotation["filename"].append(tree.xpath("//source")[0].text)
             annotation["object_id"].append(int(track.get("id")))
+            annotation["label"].append(track.get("label"))
             annotation["x_px"].append(float(x))
             annotation["y_px"].append(float(y))
             annotation["frame_id"].append(int(point.get("frame")))
@@ -172,14 +202,26 @@ def interpolate_px(df_algorithm:pd.DataFrame, n_frames:Optional[int]=None, n_obj
     df_interpolated = df_interpolated.sort_values(by=["filename" ,  "object_id", "frame_id", ]).reset_index(drop=True)
     return df_interpolated
 
-def dist_eval(df_gt, df_algorithm):
+def dist_eval(df_gt, df_algorithm, interpolate_algorithm=False):
     df_anns = df_gt
     df_anns.filename = df_anns.filename.map(lambda pth: str(Path(pth).name))
     df_algorithm.filename = df_algorithm.filename.map(lambda pth: str(Path(pth).name))
+
+    dict_all = {
+        "filename": [],
+        "x_px": [],
+        "y_px": [],
+        "x_px_gt": [],
+        "y_px_gt": [],
+        "object_id": [],
+        "frame_id": [],
+        "diff": []
+    }
     fns = []
     dst_max = []
     dst_std = []
     dst_mean = []
+    dst_median = []
     object_ids = []
     debug_per = []
     for fn in df_algorithm.filename.unique():
@@ -190,14 +232,18 @@ def dist_eval(df_gt, df_algorithm):
         object_i = 0
         object_j = 0
 
-        import ipdb
-        ipdb.set_trace()
+        # import ipdb
+        # ipdb.set_trace()
         df_an_s_fn = interpolate_px(df_anns[(df_anns.filename == fn)], n_frames=n_frames, n_objects=n_objects)
-        df_al_s_fn = interpolate_px(df_algorithm[(df_algorithm.filename == fn)], n_frames=n_frames, n_objects=n_objects)
+        if interpolate_algorithm:
+            df_al_s_fn = interpolate_px(df_algorithm[(df_algorithm.filename == fn)], n_frames=n_frames, n_objects=n_objects)
+        else:
+            df_al_s_fn = df_algorithm[(df_algorithm.filename == fn)]
         for object_i in range(n_objects):
             dist_mean = None
             dist_min = 0
             dist_std = 0
+            dist_median = 0
             for object_j in range(n_objects):
 
                 df_an_s = df_an_s_fn[(df_an_s_fn.object_id==object_j)]
@@ -214,12 +260,14 @@ def dist_eval(df_gt, df_algorithm):
                         dist_mean = np.mean(per_fram_dist)
                         dist_std = np.std(per_fram_dist)
                         dist_max = np.max(per_fram_dist)
+                        dist_median = np.median(per_fram_dist)
     #                     print(f"dist_mean={dist_mean} in init")
                     else:
                         if mn < dist_mean:
                             dist_mean = np.mean(per_fram_dist)
                             dist_std = np.std(per_fram_dist)
                             dist_max = np.max(per_fram_dist)
+                            dist_median = np.median(per_fram_dist)
     #                         print(f"dist_mean={dist_mean} in else")
             fns.append(fn)
             dst_max.append(dist_max)
@@ -227,6 +275,7 @@ def dist_eval(df_gt, df_algorithm):
             dst_mean.append(dist_mean)
     #         print(f"dist_mean={dist_mean}")
             object_ids.append(object_i)
+            dst_median.append(dist_median)
 
     df = pd.DataFrame(dict(filename=fns, object_id=object_ids, dist_max=dst_max, dist_std=dst_std, dist_mean=dst_mean))
     return df
@@ -235,8 +284,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("path", help="display a square of a given number",
                         type=str)
+    parser.add_argument("--dataset-path", help="path to dataset",
+                        default=str(os.getenv('ZDO_PATH_', default=Path(__file__).parent / 'test_dataset/')),
+                        type=str)
     args = parser.parse_args()
     sys.path.insert(0, str(Path(args.path).absolute()))
+    dataset_path = Path(args.dataset_path)
 
     import zdo2022.main
 
